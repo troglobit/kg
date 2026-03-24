33ea6ac2ceef3fd63aa8277e4063ebdb1961b8a1
Joachim Wiberg <troglobit@gmail.com>
2026-03-24 08:27:55 +0100

    Reformat help buffer for legibility
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- help.c
+++ help.c
@@ -3,23 +3,35 @@
 #include <string.h>
 #include "def.h"
 
+/*
+ * 3-column table, 79 chars wide.
+ * Each cell: 1 leading space + 9-char key field + 15-char desc field = 25 chars.
+ * Row:  │<cell25>│<cell25>│<cell25>│  = 4 + 75 = 79 chars.
+ * Sep:  ├<25─>┼<25─>┼<25─>┤          = 79 chars.
+ */
 static const char *help_lines[] = {
-	" kg -- key bindings  (press any key to return)",
-	"",
-	" MOVEMENT              EDITING               FILE             BUFFERS",
-	" C-f/C-b   fwd/back    BS        backspace   C-x C-s  save    C-x C-f  open file",
-	"                                            C-x s    save all buffers",
-	" C-n/C-p   down/up     DEL/C-d   del fwd     C-x C-c  quit    C-x b    select buf",
-	" C-a/C-e   bol/eol     C-k       kill line   C-s/C-r  search  C-x k    kill buf",
-	" C-v/M-v   pgdn/pgup   C-y       yank        C-_      undo    C-x C-b  buf list",
-	" M-f/M-b   word fw/bk  C-w       kill region C-l      redraw  C-x C-r  open r/o",
-	" C-up/dn   paragraph  M-w       copy region          C-x C-q toggle r/o",
-	" C-HOME    beg file    M-w       copy region C-h      help    C-z  suspend",
-	" C-END     end file    C-SPC     set mark    M-g      goto line",
-	"",
-	" WINDOWS",
-	" C-x 2     split horiz C-x 3     split vert  C-x o    other win",
-	" C-x 0     del window  C-x 1     del others",
+	"┌─ key bindings ─────────────────────────────────── press any key to dismiss ─┐",
+	"├─────────────────────────┬─────────────────────────┬─────────────────────────┤",
+	"│ NAVIGATION              │ EDITING                 │ FILES & BUFFERS         │",
+	"├─────────────────────────┼─────────────────────────┼─────────────────────────┤",
+	"│ C-f/C-b  fwd/back       │ BS/C-d   del back       │ C-x C-s  save           │",
+	"│ C-n/C-p  dn/up          │ DEL      del fwd        │ C-x s    save all       │",
+	"│ C-a/C-e  bol/eol        │ C-k      kill line      │ C-x C-f  open file      │",
+	"│ C-v/M-v  page dn/up     │ C-y      yank           │ C-x C-r  open r/o       │",
+	"│ M-f/M-b  word fw/bk     │ C-w      kill region    │ C-x C-q  read-only      │",
+	"│ C-up/dn  paragraph      │ M-w      copy region    │ C-x C-c  quit           │",
+	"│ C-Home   beg of file    │ C-SPC    set mark       │ C-x b    sel buffer     │",
+	"│ C-End    end of file    │ C-_      undo           │ C-x k    kill buffer    │",
+	"│ M-g      goto line      │                         │ C-x C-b  list bufs      │",
+	"├─────────────────────────┼─────────────────────────┼─────────────────────────┤",
+	"│ SEARCH                  │ MISC                    │ WINDOWS                 │",
+	"├─────────────────────────┼─────────────────────────┼─────────────────────────┤",
+	"│ C-s      search fwd     │ C-g      cancel         │ C-x 2    split horiz    │",
+	"│ C-r      search bk      │ C-l      redraw         │ C-x 3    split vert     │",
+	"│                         │ C-h      help           │ C-x o    other window   │",
+	"│                         │ C-z      suspend        │ C-x 0    del window     │",
+	"│                         │                         │ C-x 1    del others     │",
+	"└─────────────────────────┴─────────────────────────┴─────────────────────────┘",
 	NULL
 };
 
@@ -30,14 +42,22 @@ void editorToggleHelp(void)
 
 void editorDrawHelp(struct abuf *ab, int nrows)
 {
-	int nhelp = sizeof(help_lines) / sizeof(help_lines[0]) - 1; /* exclude NULL */
-	int i, len;
+	int nhelp = sizeof(help_lines) / sizeof(help_lines[0]) - 1;
+	int i;
 
 	for (i = 0; i < nrows; i++) {
 		if (i < nhelp) {
-			len = strlen(help_lines[i]);
-			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, help_lines[i], len);
+			/* UTF-8 aware clip: count display columns, not bytes.
+			 * Box-drawing and other multi-byte chars are single-width,
+			 * so we count non-continuation bytes as one column each. */
+			const char *s = help_lines[i];
+			const char *p = s;
+			int cols = 0;
+			while (*p && cols < E.screencols) {
+				if (((unsigned char)*p & 0xC0) != 0x80) cols++;
+				p++;
+			}
+			abAppend(ab, s, p - s);
 		}
 		abAppend(ab, "\x1b[0K\r\n", 6);
 	}
